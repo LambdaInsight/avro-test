151fd59fbf40d7c1e107dd36ee44bcd5f87b0123
Istvan <istvan@lambdainsight.com>
2019-12-09 20:51:27 +0100

    Initial #7

--- Program.fs
+++ Program.fs
@@ -1,51 +1,14 @@
 ﻿open System
-open Avro
-open Avro.Specific
 open System.IO
-open Avro.IO
-open Avro.Generic
+open Microsoft.Hadoop.Avro.Container
+open Microsoft.Hadoop.Avro.Utils
+open Microsoft.Hadoop.Avro
 
-
-// type HitType = {
-//   hitHostname                                   : string;
-//   hitMemoryUsed                                 : float;
-//   hitPage                                       : string;
-// }
-
-
-type HitType() =
-    let schema = Schema.Parse("""{
-  "namespace": "com.lambdainsight.hit",
-  "type": "record",
-  "name": "Hit",
-  "fields": [
-     {"name": "hitHostname",   "type": "string" },
-     {"name": "hitMemoryUsed", "type": "float"  },
-     {"name": "hitPage",       "type": "string" }
-  ]
- }""")
-
-    member val hitHostname = "" with get, set
-    member val hitMemoryUsed = 0.f with get, set
-    member val hitPage = "" with get, set
-
-    interface ISpecificRecord with
-        member this.Schema with get() = schema
-        member this.Get(fieldPos : int) = 
-            match fieldPos with
-            | 0 -> this.hitHostname :> obj
-            | 1 -> this.hitMemoryUsed :> obj
-            | 2 -> this.hitPage :> obj
-            | _ -> raise (AvroRuntimeException(sprintf "Bad index %i in Get()" fieldPos))
-        member this.Put(fieldPos : int, fieldValue : obj) = 
-            match fieldPos with
-            | 0 -> this.hitHostname <- string fieldValue
-            | 1 -> this.hitMemoryUsed <- fieldValue :?> single
-            | 2 -> this.hitPage <- string fieldValue
-            | _ -> raise (AvroRuntimeException(sprintf "Bad index %i in Put()" fieldPos))
-  
-
-
+type HitType = {
+  hitHostname                                   : string;
+  hitMemoryUsed                                 : float;
+  hitPage                                       : string;
+}
 
 let schemaJson =
   let streamReader =
@@ -54,47 +17,54 @@ let schemaJson =
     streamReader.ReadToEnd().ToString()
   fileContent
 
-let avroSchema =
-  let schemaParsed =
-    Schema.Parse(schemaJson)
-  schemaParsed
+// let avroSchema =
+//   let schemaParsed =
+//     Schema.Parse(schemaJson)
+//   schemaParsed
 
-let specificDatumWriter : SpecificDatumWriter<HitType> =
-  SpecificDatumWriter(avroSchema)
+// let specificDatumWriter : SpecificDatumWriter<HitType> =
+//   SpecificDatumWriter(avroSchema)
 
-let getAvroMsg msg =
-  let memStream = new MemoryStream(256)
-  let encoder = BinaryEncoder(memStream)
-  specificDatumWriter.Write(msg, encoder)
-  encoder.Flush()
-  memStream.ToArray()
+// let getAvroMsg msg =
+//   let memStream = new MemoryStream(256)
+//   let encoder = BinaryEncoder(memStream)
+//   specificDatumWriter.Write(msg, encoder)
+//   encoder.Flush()
+//   memStream.ToArray()
 
-let hitEmpty =
-  HitType()
+// let hitEmpty =
+//   HitType()
 
-let getJsonFromAvro (avroBytes: byte []) =
-  Console.WriteLine("{0}", avroBytes.Length)
-  let memStream = new MemoryStream(avroBytes.Length)
-  Console.WriteLine("Capacity {0} : Length {1} : {2}", memStream.Capacity, memStream.Length, memStream.Position)
-  memStream.Write(avroBytes, 0, avroBytes.Length)
-  Console.WriteLine("Capacity {0} : Length {1} : {2}", memStream.Capacity, memStream.Length, memStream.Position)
-  memStream.Seek(int64(0), SeekOrigin.Begin) |> ignore
-  Console.WriteLine("Capacity {0} : Length {1} : {2}", memStream.Capacity, memStream.Length, memStream.Position)
-  let reader = new SpecificDatumReader<HitType>(avroSchema,avroSchema)
-  let decoder = BinaryDecoder(memStream)
-  let ret = reader.Read(hitEmpty,decoder)
-  memStream.Close()
-  ret
+// let getJsonFromAvro (avroBytes: byte []) =
+//   Console.WriteLine("{0}", avroBytes.Length)
+//   let memStream = new MemoryStream(avroBytes.Length)
+//   Console.WriteLine("Capacity {0} : Length {1} : {2}", memStream.Capacity, memStream.Length, memStream.Position)
+//   memStream.Write(avroBytes, 0, avroBytes.Length)
+//   Console.WriteLine("Capacity {0} : Length {1} : {2}", memStream.Capacity, memStream.Length, memStream.Position)
+//   memStream.Seek(int64(0), SeekOrigin.Begin) |> ignore
+//   Console.WriteLine("Capacity {0} : Length {1} : {2}", memStream.Capacity, memStream.Length, memStream.Position)
+//   let reader = new SpecificDatumReader<HitType>(avroSchema,avroSchema)
+//   let decoder = BinaryDecoder(memStream)
+//   let ret = reader.Read(hitEmpty,decoder)
+//   memStream.Close()
+//   ret
 
 let testAvroFrag =
   File.ReadAllBytes("example.avro")
 
+let avroSerde = AvroSerializer.Create<HitType>()
+
+let exampleHit = { hitHostname="google.com"; hitMemoryUsed=0.123; hitPage="index.html" }
+
 [<EntryPoint>]
 let main argv =
-  Console.WriteLine("Hello World from F#!")
-  //let hit = { hitHostname="google.com"; hitMemoryUsed=0.3455; hitPage="index.html"; }
-  // let avroMsg = getAvroMsg hit
-  //Console.WriteLine hit
-  let frag = getJsonFromAvro testAvroFrag
-  Console.WriteLine("{0} : {1} : {2}", frag.hitHostname, frag.hitMemoryUsed, frag.hitPage)
+  let buffer = new MemoryStream()
+  Console.WriteLine("Capacity {0} : Length {1} : {2}", buffer.Capacity, buffer.Length, buffer.Position)
+  avroSerde.Serialize(buffer, exampleHit)
+  Console.WriteLine("Capacity {0} : Length {1} : {2}", buffer.Capacity, buffer.Length, buffer.Position)
+  buffer.Seek(int64(0), SeekOrigin.Begin) |> ignore
+  Console.WriteLine("Capacity {0} : Length {1} : {2}", buffer.Capacity, buffer.Length, buffer.Position)
+  // buffer.Write(testAvroFrag, 0, testAvroFrag.Length)
+  let frag = avroSerde.Deserialize(buffer)
+  Console.WriteLine(frag)
   0
