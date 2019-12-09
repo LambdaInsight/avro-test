f6a28113c5e96e76392b3497c4aa7a62c06e30e5
Istvan <istvan@lambdainsight.com>
2019-12-09 17:55:53 +0100

    Initial #3

--- Program.fs
+++ Program.fs
@@ -3,6 +3,7 @@ open Avro
 open Avro.Specific
 open System.IO
 open Avro.IO
+open Avro.Generic
 
 type HitType = {
   hitHostname                                   : string;
@@ -26,17 +27,35 @@ let specificDatumWriter : SpecificDatumWriter<HitType> =
   SpecificDatumWriter(avroSchema)
 
 let getAvroMsg msg =
-  let memStream =
-    new MemoryStream(256)
-  let encoder =
-    BinaryEncoder(memStream)
+  let memStream = new MemoryStream(256)
+  let encoder = BinaryEncoder(memStream)
   specificDatumWriter.Write(msg, encoder)
   encoder.Flush()
   memStream.ToArray()
 
+let hitEmpty =
+  {hitHostname = ""; hitMemoryUsed = 0.0; hitPage = ""}
+
+let getJsonFromAvro (avroBytes: byte []) =
+  Console.WriteLine("{0}", avroBytes.Length)
+  let memStream = new MemoryStream(avroBytes.Length)
+  Console.WriteLine("{0} : {1}", memStream.Capacity, memStream.Length)
+  memStream.Write(avroBytes, 0, avroBytes.Length)
+  Console.WriteLine("{0} : {1}", memStream.Capacity, memStream.Length)
+  let reader = new GenericDatumReader<HitType>(avroSchema,avroSchema)
+  let decoder = BinaryDecoder(memStream)
+  let ret = reader.Read(hitEmpty,decoder)
+  memStream.Close()
+  ret
+
+let testAvroFrag =
+  File.ReadAllBytes("example.avro")
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    let hit = {hitHostname=""; hitMemoryUsed=0.3455; hitPage=""; }
-    let avroMsg = getAvroMsg hit
-    0
+  Console.WriteLine("Hello World from F#!")
+  let hit = { hitHostname="google.com"; hitMemoryUsed=0.3455; hitPage="index.html"; }
+  // let avroMsg = getAvroMsg hit
+  Console.WriteLine hit
+  Console.WriteLine (getJsonFromAvro testAvroFrag)
+  0
---
+++ README.md
@@ -0,0 +1,18 @@
+# Avro
+
+- creating Avro frag
+
+```
+avro-tools jsontofrag --schema-file avsc.json example.json > example.avro
+```
+
+- reading Avro frag
+
+```
+avro-tools fragtojson --schema-file avsc.json example.avro
+```
+
+- .NET version
+
+```
+```
